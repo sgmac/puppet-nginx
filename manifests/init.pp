326aefbce43443e900e88d6ff6be3b88c25e19af
Sergio Galvan <sergalma@gmail.com>
2012-07-26 00:29:25 +0200

    Puppet style guide

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,79 +1,102 @@
-
+# Class: nginx
+#
+# This module installs Nginx and its default configuration using rvm as the provider.
+#
+# Parameters:
+#   $ruby_version
+#       Ruby version to install.
+#   $passenger_version
+#      Passenger version to install.
+#   $logdir
+#      Nginx's log directory.
+#   $installdir
+#      Nginx's install directory.
+#   $www
+#      Base directory for
+# Actions:
+#
+# Requires:
+#    puppet-rvm
+#
+# Sample Usage:  include nginx
 class nginx (
+  $ruby_version = 'ruby-1.9.3-p125',
+  $passenger_version = '3.0.12',
+  $logdir = '/var/log/nginx',
+  $installdir = '/opt/nginx',
+  $www    = '/var/www' ) {
 
-	$ruby_version = 'ruby-1.9.3-p125',
-	$passenger_version = '3.0.12',
-	$logdir = '/var/log/nginx',
-	$installdir = '/opt/nginx',	
-	$www	    = '/var/www',) {	
+    $options = "--auto --auto-download  --prefix=${installdir}"
+    $dependencies_passenger = [ 'libcurl4-openssl-dev' ]
 
-	$options = "--auto --auto-download  --prefix=$installdir" 
+    include rvm
 
-	include rvm
-	rvm_system_ruby {  
-		"$ruby_version":
-			ensure      => 'present',
-			default_use => true;
-	}
-	rvm_gem {
-		"$ruby_version/passenger":
-	 		ensure => "$passenger_version",
-	}
+    package { $dependencies_passenger: ensure => present }
 
-	exec {"create container":
-		command => "/bin/mkdir $www && /bin/chown www-data:www-data $www",
-		unless  => "/usr/bin/test -d $www",
-		before  => Exec['nginx-install']
-	}
+    rvm_system_ruby {
+      $ruby_version:
+        ensure      => 'present',
+        default_use => true;
+    }
 
-	exec { 'nginx-install':
-		command => "/bin/bash -l -i -c \"/usr/local/rvm/gems/$ruby_version/bin/passenger-install-nginx-module $options\"",
-		group   => 'root',
-		unless  => "/usr/bin/test -d $installdir",
-		require => [ Rvm_system_ruby["$ruby_version"], Rvm_gem["$ruby_version/passenger"]];
-	}
+    rvm_gem {
+      "${ruby_version}/passenger":
+        ensure => $passenger_version,
+    }
 
-	file { 'nginx-config':
-		path    => "$installdir/conf/nginx.conf",
-		owner   => 'root',
-		group   => 'root',
-		mode    => '0644',
-		content => template('nginx/nginx.conf.erb'),
-		require => Exec['nginx-install'],
-	}
-	
-	exec { 'create sites-conf':,
-		path    => ['/usr/bin','/bin'],
-		unless  => "/usr/bin/test -d  ${installdir}/conf/sites-available && /usr/bin/test -d ${installdir}/conf/sites-enabled",
-		command => "/bin/mkdir  ${installdir}/conf/sites-available && /bin/mkdir ${installdir}/conf/sites-enabled",
-		require => Exec['nginx-install']
-	}
+    exec { 'create container':
+      command => "/bin/mkdir ${www} && /bin/chown www-data:www-data ${www}",
+      unless  => "/usr/bin/test -d ${www}",
+      before  => Exec['nginx-install']
+    }
 
-	file { 'nginx-service':
-		path      => '/etc/init.d/nginx',
-		owner     => 'root',
-		group     => 'root',
-		mode      => '0755',
-		content   => template('nginx/nginx.erb'),
-		require   => File['nginx-config'],
-		subscribe => File['nginx-config'],
-	}
+    exec { 'nginx-install':
+      command => "/bin/bash -l -i -c \"/usr/local/rvm/gems/${ruby_version}/bin/passenger-install-nginx-module ${options}\"",
+      group   => 'root',
+      unless  => "/usr/bin/test -d ${installdir}",
+      require => [ Package[$dependencies_passenger], Rvm_system_ruby[$ruby_version], Rvm_gem["${ruby_version}/passenger"]];
+    }
 
-	file { "$logdir":
-		ensure => directory,
-		owner  => 'root',
-		group  => 'root',
-		mode   => '0644'
-	}
+    file { 'nginx-config':
+      path    => "${installdir}/conf/nginx.conf",
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      content => template('nginx/nginx.conf.erb'),
+      require => Exec['nginx-install'],
+    }
 
-	service { 'nginx':
-		ensure     => 'running',
-		enable     => true,
-		hasrestart => true,
-		hasstatus  => true,
-		subscribe  => File['nginx-config'],
-		require    => [ File["$logdir"], File['nginx-service']],
-	}
+    exec { 'create sites-conf':
+      path    => ['/usr/bin','/bin'],
+      unless  => "/usr/bin/test -d  ${installdir}/conf/sites-available && /usr/bin/test -d ${installdir}/conf/sites-enabled",
+      command => "/bin/mkdir  ${installdir}/conf/sites-available && /bin/mkdir ${installdir}/conf/sites-enabled",
+      require => Exec['nginx-install'],
+    }
+
+    file { 'nginx-service':
+      path      => '/etc/init.d/nginx',
+      owner     => 'root',
+      group     => 'root',
+      mode      => '0755',
+      content   => template('nginx/nginx.init.erb'),
+      require   => File['nginx-config'],
+      subscribe => File['nginx-config'],
+    }
+
+    file { $logdir:
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644'
+    }
+
+    service { 'nginx':
+      ensure     => running,
+      enable     => true,
+      hasrestart => true,
+      hasstatus  => true,
+      subscribe  => File['nginx-config'],
+      require    => [ File[$logdir], File['nginx-service']],
+    }
 
 }
-
--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,57 +1,72 @@
-
+# Define: nginx::vhost
+#
+# Creates nginx virtual hosts
+#
+# Parameters:
+#   $host
+#       The title of the resource  is used as the host.
+#   $port
+#       Virtual host port
+#   $root
+#       Virtual host path
+#   $create_root
+#       True or false, allows to create the path for the virtual host
+#   $rails
+#       True or false, sets if the application is rails based or not.
+# Actions:
+#       Creates a virtual host
+#
+# Requires:
+#       nginx
+#
+# Sample Usage:
+#
+#  nginx::vhost { 'test':
+#    sever_name =>  'blog.test.com'
+# }
 define nginx::vhost(
-	$host = $name,
-	$port = '80',
-	$root    = "/var/www/$host",
-	$create_root = true,
-	$server_name = "$host",
-	$rails = false,
-) {
+  $host = $name,
+  $port = '80',
+  $root    = "/var/www/${host}",
+  $makeroot = true,
+  $rails = false,
+){
+  include nginx
 
-	include nginx
+  if $makeroot{
+    file { $root:
+      ensure  => directory,
+      owner   => 'www-data',
+      group   => 'www-data',
+      mode    => '0755',
+      require => Class['nginx'],
+    }
+  }
 
-	
-	if $create_root{
-	file { "$root":
-		ensure  => directory,
-		owner   => 'www-data',
-		group   => 'www-data',
-		mode    => '0755',
-		require => Class['nginx'],
-	}
-	}
+  $template =  $rails ? {
+    true    => 'vhost.rails.erb',
+    default => 'vhost.erb',
+  }
 
-	$template =  $rails ? {
-	        true    => 'vhost.rails.erb',
-	        default => 'vhost.erb',
-	}
+  file { $host:
+    ensure  => present,
+    path    => "${nginx::installdir}/conf/sites-available/${host}",
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("nginx/${template}"),
+    require => Class['nginx'],
+  }
 
-	#	exec { "create sites $host":,
-	#		path    => ['/usr/bin','/bin'],
-	#		unless  => "/usr/bin/test -d  ${nginx::installdir}/conf/sites-available && /usr/bin/test -d ${nginx::installdir}/conf/sites-enabled",
-	#		command => "/bin/mkdir  ${nginx::installdir}/conf/sites-available && /bin/mkdir ${nginx::installdir}/conf/sites-enabled",
-	#		require => Class['nginx']
-	#	}
+  file { "${nginx::installdir}/conf/sites-enabled/${host}":
+    ensure  => link,
+    target  => "${nginx::installdir}/conf/sites-available/${host}",
+    require => File[$host],
+  }
 
-	file { "$host":
-		ensure  => present,
-		path    => "${nginx::installdir}/conf/sites-available/$host",
-		owner   => 'root',
-		group   => 'root',
-		mode    => '0644',
-		content => template("nginx/$template"),
-		require => Class['nginx'],
-	}
+  exec { "nginx ${host}":
+    command => '/etc/init.d/nginx restart',
+    require => File["${nginx::installdir}/conf/sites-enabled/${host}"],
+  }
 
-	file { "${nginx::installdir}/conf/sites-enabled/$host":
-		ensure  => link,
-		target  => "${nginx::installdir}/conf/sites-available/$host",
-		require => File["$host"],
-	}
-
-	exec {"nginx $host":
-		command => '/etc/init.d/nginx restart',
-		require => File["${nginx::installdir}/conf/sites-enabled/$host"],
-	}
-	
 }
